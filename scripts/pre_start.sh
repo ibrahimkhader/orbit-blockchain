8f8f427522456518aa39afd07bcc5191125bea6b
ibrahim <i.khader@tigercrypto.uk>
2024-09-06 14:12:24 +0500

    updated

--- scripts/pre_start.sh
+++ scripts/pre_start.sh
@@ -1,12 +1,18 @@
-version: 0.0
-os: linux
-files:
-- source: /
-    destination: /var/app
-file_exists_behavior: OVERWRITE
-hooks:
-BeforeInstall:
-    - location: scripts/pre_start.sh
-    timeout: 300
-    runas: root
+#!/bin/bash
+# export environment variables
+# echo exporting .env file
+cd ..
+# source .env
+# compile the code
+echo Compiling the code
+if ! make geth; then
+  echo "Compilation failed!"
+  exit 1
+fi
+
+if ! -d node-dir; then
+  ./build/bin/geth account new --datadir ./node-dir
+fi
+
+
 
